407d0c8dd6c8c8396614997206c332a4e99d6a5c
jtorova5 <jtorova5@gmail.com>
2024-05-03 06:40:24 -0500

    Se pasa el código a html y se deja en JS solo el código que va a variar

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,9 @@
 
-// Header section
-const header = document.querySelector('header')
-
-header.innerHTML = (`
-<nav class=" container d-flex flex-wrap justify-content-md-between pt-4 mb-4 align-items-center my-auto">
-  <div class="col-md-3 mb-2 mb-md-0 text-center align-items-baseline">
-    <a href="/" class="text-decoration-none">
-        <i class="fa-solid fa-shield-dog fa-xl text-info fs-1"></i>
-    </a>
-  </div>
-
-    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
-        <li><a href="#" class="nav-link px-2 text-info">Features</a></li>
-        <li><a href="#" class="nav-link px-2 text-info">Pricing</a></li>
-        <li><a href="#" class="nav-link px-2 text-info">FAQs</a></li>
-        <li><a href="#" class="nav-link px-2 text-info">About</a></li>
-    </ul>
-
-  <div class="col-md-3 text-end">
-    <button type="button" class="btn btn-outline-info me-2">Login</button>
-    <button type="button" class="btn btn-info text-white">Sign-up</button>
-  </div>
-</nav>
-`)
-
 // Main section
-const main = document.querySelector('main')
+const main = document.querySelector('#carousel')
 
 for (const pet of pets) {
-    main.innerHTML += (`
+    carousel.innerHTML += (`
     <figure class="card m-5 text-bg-light border-secondary border-2 rounded-3 w-25">
         <img src="${pet.img}" class="card-img-top object-fit-fill h-100" alt="${pet.petBreed}">
 
@@ -48,35 +22,3 @@ for (const pet of pets) {
     </figure>
 `)
 }
-
-// Footer section
-const footer = document.querySelector('footer')
-
-footer.innerHTML = (`
-<section class="container d-flex justify-content-center">
-    <div class="col-md-4 d-flex align-items-center justify-content-center">
-        <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-            <i class="fa-solid fa-shield-dog fa-xl text-info fs-1"></i>
-        </a>
-        <span class="mb-3 mb-md-0 text-info">&copy; 2024 Company, Inc</span>
-    </div>
-
-    <ul class="nav col-md-4 justify-content-center list-unstyled d-flex fs-3">
-        <li class="ms-3">
-            <a class="text-info" href="https://www.facebook.com/CielomioATV/" target="_blank">
-                <i class="bi bi-facebook"></i>
-            </a>
-        </li>
-        <li class="ms-3">
-            <a class="text-info" href="https://www.instagram.com/cielomiow/?ref=beben&hl=en" target="_blank">
-                <i class="bi bi-instagram"></i>
-            </a>
-        </li>
-        <li class="ms-3">
-            <a class="text-info" href="https://api.whatsapp.com/message/NLPW5ZMO4MYPG1?autoload=1&app_absent=0" target="_blank">
-                <i class="bi bi-whatsapp"></i>
-            </a>
-        </li>
-    </ul>
-</section>
-`)
